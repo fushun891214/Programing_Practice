9572df405b4a9e0ee35b1b83fc427378733239d6
fushun <102985209+fushun891214@users.noreply.github.com>
2026-01-24 22:06:54 +0800

    feat:LeetCode 82

---
+++ LeetCode/82. Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *p = dummy;
+        ListNode *q = head;
+        while(q != NULL){
+            if((q->next != NULL) && (q->val == q->next->val)){
+                while((q->next != NULL) && (q->val == q->next->val)){
+                    q = q->next;
+                }
+                q = q->next;
+                if(q == NULL){
+                    p->next = NULL;
+                }
+            }
+            else{
+                p->next = q;
+                p = p->next;
+                q = q->next;
+            }
+        }
+        return dummy->next;
+    }
+};
+
+// 另一種解法
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *dummy1 = new ListNode(101);
+        ListNode *dummy2 = new ListNode(101);
+        ListNode *d1 = dummy1;
+        ListNode *d2 = dummy2;
+        ListNode *p = head;
+        while(p != NULL){
+            if((p->next != NULL) && (p->val == p->next->val) || (p->val == d1->val)){
+                d1->next = p;
+                d1 = d1->next;
+            }
+            else{
+                d2->next = p;
+                d2 = d2->next;
+            }
+            p = p->next;
+            d1->next = NULL;
+            d2->next = NULL;
+        }
+        return dummy2->next;
+    }
+};
